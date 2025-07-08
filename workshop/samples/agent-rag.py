b9305803bd9a8228c50a3808bb7e4ddb31e40ff2
Alec Cunningham <acunningham@drwholdings.com>
2025-07-08 12:51:16 -0500

    feat: switch to local pdfs, provide example data

--- workshop/samples/agent-rag.py
+++ workshop/samples/agent-rag.py
@@ -2,18 +2,27 @@ import os
 from dotenv import load_dotenv
 from agno.agent import Agent
 from agno.models.openai import OpenAIChat
-from agno.knowledge.pdf import PDFKnowledgeBase
+from agno.knowledge.pdf import PDFKnowledgeBase, PDFReader
 from agno.vectordb.pgvector import PgVector, SearchType
 
 load_dotenv()
 
 DB_URL = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
-knowledge_base = PDFUrlKnowledgeBase(
-    urls=["$RETRIEVED FROM WORKSHOP ASSETS"],
-    vector_db=PgVector(table_name="financial_reports", db_url=DB_URL, search_type=SearchType.hybrid),
+# Retrieve PDF's from any of the following sources:
+# https://investor.nvidia.com/financials/
+# https://www.sec.gov/edgar/browse/?CIK=1045810
+# https://www.nasdaq.com/market-activity/stocks/nvda/sec-filings
+
+knowledge_base = PDFKnowledgeBase(
+    path="data/pdfs", # <-- Relative path to downloaded PDF's
+    vector_db=PgVector(
+        table_name="financial_reports", 
+        db_url=DB_URL, 
+        search_type=SearchType.hybrid,
+    ),
+    reader=PDFReader(chunk=True)
 )
-knowledge_base.load(load=True,upsert=True)
 
 agent = Agent(
     model=OpenAIChat(id="gpt-4o", api_key=os.getenv("OPENAI_API_KEY")),
@@ -22,4 +31,6 @@ agent = Agent(
     markdown=True,
 )
 
-agent.print_response("What what NVDA's quarterly revenue in 2024?", stream=True)
+agent.knowledge.load(recreate=True)
+
+agent.print_response("What was NVIDA's quarterly revenue in 2025?", stream=True)
